ba3817134a2662c1e5f054ce78ca77216b4d3d52
Alander Machado <alandermm@hotmail.com>
2018-02-07 19:58:51 -0200

    first commit

---
+++ Dados/AutenticacaoContext.cs
@@ -0,0 +1,22 @@
+using autenticacaoEfCookie.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace autenticacaoEfCookie.Dados
+{
+    public class AutenticacaoContext:DbContext
+    {
+        public AutenticacaoContext(DbContextOptions<AutenticacaoContext> options):base(options){}
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Permissao> Permissoes { get; set; }
+        public DbSet<UsuarioPermissao> UsuarioPermissoes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<Usuario>().ToTable("Usuarios");
+            modelBuilder.Entity<Permissao>().ToTable("Permissoes");
+            modelBuilder.Entity<UsuarioPermissao>().ToTable("UsuariosPermissoes");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
